5f0b73412faa37d7cbecdd9b1c6d748c43942d3c
Anton Arapov <anton@descope.org>
2014-04-22 17:20:18 +0200

    app: fix malforms in get-raw.php
    strange has happened to this file during open-sourcing. fix it.
    
    Signed-off-by: Anton Arapov <anton@descope.org>

--- php/get-raw.php
+++ php/get-raw.php
@@ -1,20 +1,20 @@
-<?Php
-Require "wp-Load.php";
-Ini_set('Memory_limit', '1024m');
-Global $Wpdb;
-$List = Mysql_fetch_array(Mysql_query("Select Meta_value as Rawlist from Wp_postmeta Where Meta_key='Wpcf-raw-List' and Post_id=" . Htmlspecialchars($_get['id'])));
-$Token = Hash_init("Sha256");
-Hash_update($Token, $List["Rawlist"]);
-$Sec_token = Hash_final($Token);
-If ($Sec_token != $_get['Token']) {
-	Echo "Sorry";
-	Exit();
+<?php
+require "wp-load.php";
+ini_set('memory_limit', '1024M');
+global $wpdb;
+$list = mysql_fetch_array(mysql_query("SELECT meta_value AS rawlist FROM wp_postmeta WHERE meta_key='wpcf-raw-list' AND post_id=" . htmlspecialchars($_GET['id'])));
+$token = hash_init("sha256");
+hash_update($token, $list["rawlist"]);
+$sec_token = hash_final($token);
+if ($sec_token != $_GET['token']) {
+    echo "Sorry";
+    exit();
 }
-$sql = "Select Distinct raw from Raw_data Where id in ( " . $List["Rawlist"] . " )";
-$Query = Mysql_query($sql);
-While ($row = Mysql_fetch_array($Query)) {
-	Echo '<pre id="Raw_content">';
-	Echo Htmlspecialchars(Obfuscateraw($row['raw']));
-	Echo '</pre>';
+$sql = "SELECT DISTINCT raw FROM raw_data WHERE id IN ( " . $list["rawlist"] . " )";
+$query = mysql_query($sql);
+while ($row = mysql_fetch_array($query)) {
+    echo '<pre id="raw_content">';
+    echo htmlspecialchars(ObfuscateRaw($row['raw']));
+    echo '</pre>';
 }
 ?>
